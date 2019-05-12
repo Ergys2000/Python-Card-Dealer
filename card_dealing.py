f0c9278a273a4b6b06adf70ef43040d6aee6f661
Kifula <rrjolligys@gmail.com>
2019-05-12 15:37:21 +0200

    Made names better and modified analasys methods

--- card_dealing.py
+++ card_dealing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import re
 
 class Mass_Card_Dealer:
 
@@ -7,11 +8,11 @@ class Mass_Card_Dealer:
         """ This class simulates multiple card dealings and with its methods you can get statistics on these card dealings"""
         self.dealings = dealings
         self.decks = [] 
-        colors = ['H', 'S', 'C', 'D']
+        colors = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
         for i in range(self.dealings):
             for j in range(13):
                 for k in range(4):
-                    self.decks.append(str(j)+colors[k])
+                    self.decks.append(str(j+1)+' of '+colors[k])
         
                 
         self.decks = np.array(self.decks).reshape((dealings, 52))
@@ -27,11 +28,13 @@ class Mass_Card_Dealer:
         n = 0
         for deck in self.decks:
             for person in deck:
-                sum = 0
+                sum=0
                 for c in person:
-                    if int(c[0]) == card:
-                        n+=1
-                if n >= frequency:
+                    card_nr = re.findall('\d+', c)
+                    nr = int(card_nr[0])
+                    if nr == card:
+                        sum +=1
+                if sum >= frequency:
                     n+=1
         return n
 
@@ -42,9 +45,11 @@ class Mass_Card_Dealer:
             for person in deck:
                 cards_met = []
                 for card in person:
-                    if card not in cards_met:
-                        if (card >= start) and (card <= end):
-                            cards_met.append(card)
+                    card_nr = re.findall('\d+', card)
+                    nr = int(card_nr[0])
+                    if nr not in cards_met:
+                        if (nr >= start) and (nr <= end):
+                            cards_met.append(nr)
                 if len(cards_met) == (end-start+1):
                     n += 1
         return n
@@ -62,40 +67,6 @@ class Mass_Card_Dealer:
         columns = [outer, inner]
         self.frame = pd.DataFrame(cards_table.T, index=index, columns=columns) # we passed the transpose because we want to 
                                                                                # have the cards as the indexes
-
-
-class Card_Simulator:
-
-
-    def __init__(self):
-
-        colors = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
-        cards = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
-        deck = {'Cards':[],'Colors':[]} 
-        for i in range(13):
-            for j in range(4):
-                deck['Cards'].append(cards[i])
-                deck['Colors'].append(colors[j])
-                
-        index = [['Person '+str(i) for i in range(4) for j in range(13)],
-                 [j for i in range(4) for j in range(13)]]
-
-        self.df = pd.DataFrame(deck, index=index)
-        for i in range(52):
-            index = np.random.randint(52)
-            card_hold = self.df.iloc[i].copy()
-            self.df.iloc[i] = self.df.iloc[index]
-            self.df.iloc[index] = card_hold
-
-    def n_who_have(self, card, frequency):
-        n=0
-        for i in range(4):
-            row_name = 'Person '+str(i)
-            person = self.df.loc[row_name]
-            nr_cards = (person.Cards == card).sum()
-            if nr_cards >= frequency:
-                n+=1
-        return n;
-x = Mass_Card_Dealer(2)
+x = Mass_Card_Dealer(1000)
 x.make_dataframe()
 print(x.frame)
